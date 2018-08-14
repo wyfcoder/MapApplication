fdc9cfda88085f823858103bbff31900e17e5e1c
Ellly <1918267340@qq.com>
2018-08-14 16:15:32 +0800

    合并完成。

--- app/src/main/java/com/example/team/mapapplication/business/background_functions/location/LocationService2.java
+++ app/src/main/java/com/example/team/mapapplication/business/background_functions/location/LocationService2.java
@@ -5,38 +5,19 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Binder;
 import android.os.IBinder;
 
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
-import com.baidu.mapapi.model.LatLng;
-import com.example.team.mapapplication.business.background_functions.sensor.SensorData;
-import com.example.team.mapapplication.business.background_functions.sensor.SensorService;
 import com.example.team.mapapplication.business.background_functions.latlng.JingWei;
 import com.example.team.mapapplication.business.background_functions.latlng.Math_ways;
-import com.example.team.mapapplication.engine.RepeatHandler;
+import com.example.team.mapapplication.business.background_functions.sensor.SensorData;
+import com.example.team.mapapplication.business.background_functions.sensor.SensorService;
 
-public class LocationService extends Service {
-
-    private IntereactionForLocation mLocationInterface;
-    private RepeatHandler mRequestHandler;
-
-    public void setLocationInterface(IntereactionForLocation i){
-        mLocationInterface = i;
-    }
-
-    public class ServiceBinder extends Binder{
-        public LocationService getService(){
-            return LocationService.this;
-        }
-    }
-
-
-
-    private ServiceBinder mBinder = new ServiceBinder();
+public class LocationService2 extends Service
+{
 
     private SensorService sensorService;//传感器的数据
 
@@ -88,7 +69,7 @@ public class LocationService extends Service {
                 old.longitude_value = longitude_value;
                 old.latitude_value = latitude_value;
 
-                walkDistance = math_ways.stepToDistance(sensorData.steps, getApplicationContext());
+                walkDistance = math_ways.stepToDistance(sensorData.steps,getApplicationContext());
 
                 JingWei newO = math_ways.countJW(old, walkDistance, sensorData.lastX);
 
@@ -103,7 +84,8 @@ public class LocationService extends Service {
                     if (latitude_last == latitude2 && longitude_last == longitude2)
                     {
                         frequeence++;
-                        if(distance<35) {
+                        if(distance<35)
+                        {
                             longitude_value = newO.longitude_value;
                             latitude_value = newO.latitude_value;
                         }
@@ -113,19 +95,18 @@ public class LocationService extends Service {
                             latitude_value=latitude_last;
                         }
                     }
-
                     else
                     {
-                        if (distance <= 25||distance>200||frequeence<=5)
-                        {
-                            longitude_value = newO.longitude_value;
-                            latitude_value = newO.latitude_value;
-                        }
-                        else
-                        {
-                            longitude_value = longitude2;
-                            latitude_value = latitude2;
-                        }
+                            if (distance <= 25||distance>200||frequeence<=5)
+                            {
+                                longitude_value = newO.longitude_value;
+                                latitude_value = newO.latitude_value;
+                            }
+                            else
+                            {
+                                longitude_value = longitude2;
+                                latitude_value = latitude2;
+                            }
                         if (distance <= 300)
                         {
                             longitude_last = longitude2;
@@ -134,7 +115,7 @@ public class LocationService extends Service {
                         frequeence=0;
                     }
                 }
-                else
+                    else
                 {
                     re_times--;
                     latitude_value = newO.latitude_value;
@@ -142,36 +123,28 @@ public class LocationService extends Service {
                     longitude_last=longitude2;
                     latitude_last=latitude2;
                 }
-                    sensorService.restart();
-                }
+                sensorService.restart();
+            }
             lastX = sensorData.lastX;
             accurate = bdLocation.getRadius();
-
-
-//            locationClient.stop();  // OK here is where all happens. I mute it and the notification stop flashing. wyy
-
-            if (mLocationInterface != null){
-                mLocationInterface.passLocation(new LatLng(latitude_value, longitude_value), accurate, lastX);
-            }
-
-//            sendBroadcast();  No need to communicate with the original "Main Activity". wyy
+            locationClient.stop();
+            sendBroadcast();
         }
     }
 
-
-    /**
-     * Thread.sleep() seems not reliable, at least on my phone MIX1 the 3-sec blank time does not exist. pity.
-     * Thus I decide to replace it with handler. wyy
-     */
-    @Deprecated
     private class DataThread implements Runnable {
         @Override
         public void run() {
             while (true) {
                 try {
-                    Thread.sleep(3000);
+                    Thread.sleep(500);
+                    locationClient = new LocationClient(getApplicationContext());
+                    locationClient.setLocOption(option);
+                    locationClient.registerLocationListener(myLocationListener);
                     locationClient.start();
-                } catch (InterruptedException e) {
+                }
+                catch (InterruptedException e)
+                {
                     e.printStackTrace();
                 }
             }
@@ -180,61 +153,26 @@ public class LocationService extends Service {
 
     private DataThread dataThread;
 
-    public LocationService() {
+    public LocationService2()
+    {
 
     }
 
 
-    public void onCreate() {
+    public void onCreate()
+    {
         super.onCreate();
         initOption();
-        initClient();
-
-
         isFirst = true;
         re_times = 0;
         sensorService = new SensorService(getApplicationContext());
         initReceiver();
         dataThread = new DataThread();
-        // here I deleted this line in order to multi-call the service to get latitude and longitude. wyy
+        new Thread(dataThread).start();
     }
 
-    /**
-     * Considering that it seems there is no need to initiate the client every time the request for location comes.
-     * Thus I have moved this code block to here to initiate it for only once
-
-     * OK I must confess that I'm wrong - .- . I don't know why after I have done such change cited above the GPS notification will crazily flash in the notification bar.
-     * Now the change is discarded. (Maybe I can find some information on the official Doc)
-
-     * OK again I must again confess I'm wrong -. - . Here is not where the problem arises from.
-     * this change will not disturb normal process. So it just comes back again.
-     * wyy
-     */
-
-    private void initClient() {
-        locationClient = new LocationClient(getApplicationContext());
-        locationClient.setLocOption(option);
-        locationClient.registerLocationListener(myLocationListener);
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        //the deleted line above has been moved here
-
-        Runnable requestLocation = new Runnable() {
-            @Override
-            public void run() {
-                locationClient.start();
-            }
-        };
-
-        mRequestHandler = new RepeatHandler(requestLocation);
-        mRequestHandler.setRepeatOn(true);
-        mRequestHandler.setMessageMark(109);
-        mRequestHandler.setDelay(1000);
-        mRequestHandler.start();
-
-//        new Thread(dataThread).start();
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -243,9 +181,9 @@ public class LocationService extends Service {
         super.onDestroy();
     }
 
-    public ServiceBinder onBind(Intent intent) {
+    public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        return mBinder;
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     private void initOption() {
@@ -253,19 +191,20 @@ public class LocationService extends Service {
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
         option.setCoorType("all");
         option.setCoorType("bd09ll");
-        option.setScanSpan(1000);  // the doc says the span must be higher than 1000 so I just turn it to 1000  * _ * wyy
+        option.setScanSpan(500);
         option.disableCache(true);
         option.setIsNeedLocationDescribe(true);
         option.setWifiCacheTimeOut(5 * 60 * 1000);
         option.setEnableSimulateGps(false);
-        option.setIgnoreKillProcess(true);
+        option.setIgnoreKillProcess(false);
         option.setLocationNotify(true);
         option.setOpenGps(true);
         locationClient = new LocationClient(getApplicationContext());
         locationClient.setLocOption(option);
     }
 
-    private void initReceiver() {
+    private void initReceiver()
+    {
         reLocationBroadcastReceive = new ReLocationBroadcastReceive();
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction("com.reLocation");
--- app/src/main/java/com/example/team/mapapplication/business/background_functions/sensor/SensorService.java
+++ app/src/main/java/com/example/team/mapapplication/business/background_functions/sensor/SensorService.java
@@ -6,6 +6,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,53 +17,18 @@ public class SensorService implements SensorEventListener
     private Context mContext;
     public double  lastX;
     double steps;
-    private Sensor stepDetector;
-    private Sensor countSensor;
     private Sensor accerate;
-    private int pre_steps;
-    private boolean isFirst;
-    private int mode;
     public List<Double> values;
-    long times;
 
     public SensorService(Context context)
     {
         mContext=context;
         values=new ArrayList<>();
-        times=0;
         steps=0;
         lastX=0;
-        isFirst=true;
-        mSensorManager= (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
-        if(mSensorManager!= null)
-        {
-            mSensor=mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-        }
-        //判断是否有方向传感器
-        if(mSensor!=null)
-        {
-            //注册监听器
-            mSensorManager.registerListener(this,mSensor, SensorManager.SENSOR_DELAY_UI);
-        }
-        stepDetector=mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);//获取单次计步传感器
-        countSensor=mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        if(stepDetector!=null)
-        {
-            mSensorManager.registerListener(this,stepDetector, SensorManager.SENSOR_DELAY_UI);
-            mode=1;
-        }
-        else if(countSensor!=null)
-        {
-            mSensorManager.registerListener(this,countSensor, SensorManager.SENSOR_DELAY_UI);
-            mode=2;
-        }
-        else
-        {
-            accerate=mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            mSensorManager.registerListener(this,accerate, SensorManager.SENSOR_DELAY_UI);
-            mode=3;
-        }
+        initSensor();
     }
+
     public void onSensorChanged(SensorEvent event)
     {
         if (event.sensor.getType() == Sensor.TYPE_ORIENTATION)
@@ -70,51 +36,31 @@ public class SensorService implements SensorEventListener
             float x = event.values[SensorManager.DATA_X];
             lastX = x;
         }
-        else if(event.sensor.getType()== Sensor.TYPE_STEP_DETECTOR)
+           else if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER)
         {
-            if(isFirst)
-            {
-                pre_steps=(int) event.values[0];
-                isFirst=false;
-            }
-            else
-                steps=event.values[0]-pre_steps;
-        }
-        else if(event.sensor.getType()== Sensor.TYPE_ACCELEROMETER)
-        {
-            double values1= Math.sqrt(event.values[0]*event.values[0]+event.values[1]*event.values[1]+event.values[2]*event.values[2]);
-            if(times>10)
+            double values1=Math.sqrt(event.values[0]*event.values[0]+event.values[1]*event.values[1]+event.values[2]*event.values[2]);
             values.add(values1);
-            else
-                times++;
-        }
-        else if(event.sensor.getType()== Sensor.TYPE_STEP_COUNTER)
-        {
-            if(isFirst)
-            {
-                steps=event.values[0];
-                isFirst=false;
-            }
-            else
-            {
-                steps+=event.values[0];
-            }
         }
     }
     public void onAccuracyChanged(Sensor sensor, int accuracy)
     {
+
     }
+
     public SensorData get()
     {
         mSensorManager.unregisterListener(this);
         SensorData sensorData=new SensorData();
-        if(mode==3&&values.size()>0)
+        if(values.size()>0)
         {
             dealSteps();
-            sensorData.steps=steps*1.5;
+            sensorData.steps=steps;
         }
-        else  sensorData.steps=steps;
-        sensorData.lastX=lastX;;
+        else
+        {
+            sensorData.steps=0;
+        }
+        sensorData.lastX=lastX;
         return sensorData;
     }
 
@@ -122,74 +68,41 @@ public class SensorService implements SensorEventListener
     {
         if(values.size()!=0)
             values.clear();
-        times=0;
         steps=0;
         lastX=0;
-        isFirst=true;
+        initSensor();
+    }
+
+    private void dealSteps()
+    {
+        List<Integer> data=new ArrayList<>();
+        for(int i=0;i<values.size();i++)  data.add(values.get(i).intValue());
+        int t=0;
+        for(int i=1;i<data.size();i++)
+        {
+            if(data.get(i-1)>data.get(i))
+                t++;
+        }
+        
+        if(t<5) steps=0;
+        else if(5<t&&t<10) steps=0.5;
+        else if(t<20&&t>10) steps=1;
+        else if(t>20) steps=1.5;
+    }
+
+    private void initSensor()
+    {
         mSensorManager= (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
         if(mSensorManager!= null)
         {
             mSensor=mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
         }
-        //判断是否有方向传感器
         if(mSensor!=null)
         {
-            //注册监听器
-            mSensorManager.registerListener(this,mSensor, SensorManager.SENSOR_DELAY_UI);
+            mSensorManager.registerListener(this,mSensor,SensorManager.SENSOR_DELAY_UI);
         }
-        switch (mode)
-        {
-            case 1:
-                stepDetector=mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
-                mSensorManager.registerListener(this,stepDetector, SensorManager.SENSOR_DELAY_UI);
-                break;
-            case 2:
-                countSensor=mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-                mSensorManager.registerListener(this,countSensor, SensorManager.SENSOR_DELAY_UI);
-                break;
-            case 3:
-                accerate=mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-                mSensorManager.registerListener(this,accerate, SensorManager.SENSOR_DELAY_UI);
-                break;
-        }
-    }
-    private void dealSteps()
-    {
-        List<Double> datas=new ArrayList<>();
-        double data=values.get(0);
-        datas.add(values.get(0));
-        for(int i=1;i<values.size();i++)
-        {
-            if(Math.abs(values.get(i)-data)>0.1*data)
-            {
-                datas.add(values.get(i));
-                data=values.get(i);
-            }
-        }
-        double average=0;
-        for(int i=0;i<datas.size();i++)
-        {
-            average+=datas.get(i);
-        }
-         average=average/(0.8*datas.size());
-        int f=0;
-        for(int i=0;i<datas.size();i++)
-        {
-            if(datas.get(i)>=average)
-            {
-                if(f>=2)
-                {
-                    steps++;
-                    f=0;
-                }
-                else
-                    f++;
-            }
-            else
-            {
-                f++;
-            }
-        }
+        accerate=mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this,accerate,SensorManager.SENSOR_DELAY_FASTEST);
     }
 
 }
